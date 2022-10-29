afeb5682a2861ab3c5687730ff44dd21e10a1fbf
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-10-29 15:12:53 +0300

    Initial commit

---
+++ src/main/java/com/example/wapizzabackend/WapizzaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.wapizzabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WapizzaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WapizzaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wapizzabackend/WapizzaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wapizzabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WapizzaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
